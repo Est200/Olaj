3d99278a71ae0e05388867ec1ab738cbf496ab21
Est200 <olajideesther2019@gmail.com>
2022-06-29 09:40:14 +0100

    init

--- I4G021888EFF/I4G021888EFF/settings.py
+++ I4G021888EFF/I4G021888EFF/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-82i5pz5g#7+ofoc@kvc-c2y&%v%s5_8yuq+*)s-q(rfz^+obz0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
